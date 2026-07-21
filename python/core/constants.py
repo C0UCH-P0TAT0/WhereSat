c7454a6157509b4f00acb7ab555a5889b4b64627
C0UCH-P0TAT0 <adityamalpani006@gmail.com>
2026-07-21 12:13:42 +0530

    Update constants.py

--- python/core/constants.py
+++ python/core/constants.py
@@ -3,8 +3,8 @@
 # ==========================================
 CAMERA_RES_X = 1024       # Pixel width
 CAMERA_RES_Y = 1024       # Pixel height
-FOV_X_DEG = 20.0          # Field of View in degrees (Horizontal)
-FOV_Y_DEG = 20.0          # Field of View in degrees (Vertical)
+FOV_X_DEG = 25.0          # Field of View in degrees (Horizontal)
+FOV_Y_DEG = 25.0          # Field of View in degrees (Vertical)
 
 # ==========================================
 # QUATERNION CONVENTION (DO NOT CHANGE)
